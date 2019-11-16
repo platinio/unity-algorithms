e8ac200cfc111fd790816c0675fb820d9b922c83
Platinio <platinio94@gmail.com>
2019-11-15 23:48:56 -0300

    add implementation of Quick Find

---
+++ Assets/QuickFind/Scripts/QuickFind.cs
@@ -0,0 +1,42 @@
+﻿
+namespace UnityAlgorithms
+{
+    public class QuickFind 
+    {
+        private int[] array;
+
+        public QuickFind(int size)
+        {
+            array = new int[size];
+
+            for (int n = 0; n < size; n++)
+            {
+                array[n] = n;
+            }
+        }
+
+        public bool IsConnected(int a , int b)
+        {
+            return array[a] == array[b];
+        }
+
+        public void CreateConnection(int a , int b)
+        {
+            int newConnection = a;
+            int oldConnection = b;
+
+            for (int n = 0; n < array.Length; n++)
+            {
+                if (array[n] == oldConnection)
+                {
+                    array[n] = newConnection;
+                }
+            }
+
+        }
+
+    }
+
+}
+
+
