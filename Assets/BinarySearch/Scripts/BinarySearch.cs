a1ed4ce33e8eddff9d32f235f5c109d6fac704fb
Platinio <platinio94@gmail.com>
2019-11-17 00:50:38 -0300

    add binary search small implementation

---
+++ Assets/BinarySearch/Scripts/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+namespace UnityAlgorithms
+{
+    public class BinarySearch 
+    {
+        public int DoABinarySearch(int[] array , int key)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + ( high - low ) / 2;
+                if (array[mid] > key)
+                {
+                    low = mid + 1;
+                }
+                else if (array[mid] < key)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    return array[mid];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
---
+++ Assets/BinarySearch/Scripts/BinarySearchTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class BinarySearchTest : MonoBehaviour
+    {
+        [SerializeField] private int[] array = null;
+        [SerializeField] private int key = 0;
+
+        private void Awake()
+        {
+            BinarySearch bs = new BinarySearch();
+            int result =  bs.DoABinarySearch(array , key);
+
+            Debug.Log("result of bs " + result);
+
+        }
+    }
+}
+
