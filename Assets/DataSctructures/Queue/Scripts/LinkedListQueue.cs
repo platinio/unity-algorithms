14cc1c3c70bcc0ea2716dbbc16d9bc777214d452
Platinio <platinio94@gmail.com>
2019-11-17 15:39:05 -0300

    add new LinkedListQueue data sctructure

---
+++ Assets/DataSctructures/Queue/Scripts/LinkedListQueue.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class LinkedListQueue<T>
+    {
+        private Node<T> first = null;
+        private Node<T> last = null;
+
+        public bool isEmpty()
+        {
+            return first == null;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> oldLast = last;
+
+            last = new Node<T>();
+            last.item = item;
+            last.nextNode = null;
+
+            if (isEmpty())
+            {
+                first = last;
+            }
+            else
+            {
+                oldLast.nextNode = last;
+            }
+            
+        }
+
+        public T Dequeue()
+        {
+            if (isEmpty())
+            {
+                return default(T);
+            }
+
+            T item = first.item;
+            first = first.nextNode;
+
+            if (isEmpty())
+            {
+                last = null;
+            }
+
+            return item;
+        }
+
+        public int Size()
+        {
+            if (first == null)
+            {
+                Debug.Log("first is null");
+                return 0;
+            }
+                
+
+            int size = 1;
+            Node<T> node = first;
+
+            if (node.nextNode == null)
+            {
+                Debug.Log("next node is null");
+            }
+
+            while (node.nextNode != null)
+            {
+                size++;
+                node = node.nextNode;
+            }
+
+            return size;
+
+        }
+
+
+
+        private class Node<T>
+        {
+            public T item;
+            public Node<T> nextNode;
+        }
+
+    }
+
+}
+
---
+++ Assets/DataSctructures/Queue/Scripts/LinkedListQueueTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class LinkedListQueueTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            LinkedListQueue<string> stringQueue = new LinkedListQueue<string>();
+            stringQueue.Enqueue("Hi");
+            stringQueue.Enqueue( "I" );
+            stringQueue.Enqueue( "am" );
+            stringQueue.Enqueue( "James Roman" );
+
+            int size = stringQueue.Size();
+            for (int n = 0; n < 100 ; n++)
+            {
+                Debug.Log( stringQueue.Dequeue() );
+            }
+
+        }
+    }
+
+}
+
