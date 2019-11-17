957a989b9b08aac711ad1d45ad743c219fe76470
Platinio <platinio94@gmail.com>
2019-11-17 17:52:52 -0300

    add Stack array implementation

---
+++ Assets/DataSctructures/Stack/Scripts/ArrayStack.cs
@@ -0,0 +1,25 @@
+﻿namespace UnityAlgorithms
+{
+    public class ArrayStack<T>
+    {
+        public T[] array = null;
+        public int head = 0;
+
+        public ArrayStack(int capacity)
+        {
+            array = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            array[head++] = item;
+        }
+
+        public T Pop()
+        {
+            return array[--head];
+        }
+
+    }
+}
+
---
+++ Assets/DataSctructures/Stack/Scripts/ArrayStackTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class ArrayStackTest : MonoBehaviour
+    {
+        
+        private void Awake()
+        {
+            ArrayStack<string> arrayStack = new ArrayStack<string>(10);
+
+            arrayStack.Push("Hola");
+            arrayStack.Push("Hi");
+            arrayStack.Push( "borra esto" );
+
+            arrayStack.Pop();
+
+            for (int n = 0; n < arrayStack.array.Length; n++)
+            {
+                Debug.Log( arrayStack.array[n] );
+            }
+            
+        }
+    }
+}
+
