77bf77540cc3b31f26d4bc9cf12d106db1aa2adc
Platinio <platinio94@gmail.com>
2019-11-22 23:47:00 -0300

    implement insertiong sort

---
+++ Assets/Algorithms/Sorting/InsertionSort/InsertionSortTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class InsertionSortTest : MonoBehaviour
+    {
+        [SerializeField] private int numbeROfEntries = 100;
+
+        private void Awake()
+        {
+            List<int> collection = new List<int>( numbeROfEntries );
+
+            for (int n = 0; n < numbeROfEntries; n++)
+            {
+                collection.Add( Random.Range( 0, numbeROfEntries ) );
+            }
+
+
+            InsertiongSort.Sort<int>( collection );
+
+            foreach (int item in collection)
+                Debug.Log( item );
+
+        }
+    }
+}
+
---
+++ Assets/Algorithms/Sorting/InsertionSort/InsertiongSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityAlgorithms
+{
+    public class InsertiongSort
+    {
+
+        public static void Sort<T>(IList<T> collection) where T : IComparable
+        {
+            for (int n = 0; n < collection.Count; n++)
+            {
+                for (int j = n ; j > 0 ; j--)
+                {
+                    if (collection[j].CompareTo( collection[j - 1] ) == -1)
+                    {
+                        Swap<T>( collection, j, j - 1 );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Swap<T>(IList<T> collection, int indexA, int indexB)
+        {
+            T valueA = collection[indexA];
+            collection[indexA] = collection[indexB];
+            collection[indexB] = valueA;
+        }
+
+
+    }
+}
+
