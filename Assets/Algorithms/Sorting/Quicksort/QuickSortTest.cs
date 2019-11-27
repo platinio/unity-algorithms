4c9ce849d0039ae7ebbed596107abb07ff6acbd1
Platinio <platinio94@gmail.com>
2019-11-27 16:47:56 -0300

    small changes over quicksort

---
+++ Assets/Algorithms/Sorting/Quicksort/QuickSortTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityAlgorithms
+{
+    public class QuickSortTest : MonoBehaviour
+    {
+        public List<int> array = new List<int>( );
+
+        private void Awake()
+        {
+            
+
+            Quicksort.Sort( array );
+
+
+            for (int n = 0; n < array.Count; n++)
+            {
+                Debug.Log( array[n] );
+            }
+
+        }
+    }
+
+}
+
--- Assets/Algorithms/Sorting/Quicksort/Quicksort.cs
+++ Assets/Algorithms/Sorting/Quicksort/Quicksort.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Quicksort : MonoBehaviour
+namespace UnityAlgorithms
 {
-    // Start is called before the first frame update
-    void Start()
+    public static class Quicksort 
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static void Sort<T>(IList<T> collection) where T : IComparable
+        {
+            Shuffling.Shuffle(collection);
+            Sort(collection , 0 , collection.Count - 1);
+        }
+
+        private static void Sort<T>(IList<T> collection , int low , int high) where T : IComparable
+        {
+            if (high <= low)
+                return;
+
+            
+            int j = Partition( collection , low , high );
+            
+            Sort(collection , low , j - 1);
+            Sort(collection , j + 1 , high);
+
+            
+        }
+
+        private static int Partition<T>( IList<T> collection , int low , int high) where T : IComparable
+        {
+            int i = low;
+            int j = high + 1;
+
+            while (true)
+            {
+                while (collection[++i].CompareTo( collection[low] ) == -1)
+                {
+                    if (i == high)
+                    {
+                        break;
+                    }
+                }
+
+                while (collection[low].CompareTo( collection[--j] ) == -1)
+                {
+                    if (j == low)
+                    {
+                        break;
+                    }
+                }
+
+                if (i >= j)
+                {
+                    break;
+                }
+
+                Swap(collection , i , j);
+
+            }
+
+            Swap(collection , low , j);
+            return j;
+
+        }
+
+        private static void Swap<T>(IList<T> collection, int indexA, int indexB)
+        {
+            T valueA = collection[indexA];
+            collection[indexA] = collection[indexB];
+            collection[indexB] = valueA;
+        }
     }
 }
+
