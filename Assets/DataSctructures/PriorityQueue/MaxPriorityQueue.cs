80f5ce0665684d0d288a282e145aec265e31911a
Platinio <platinio94@gmail.com>
2019-11-27 17:40:32 -0300

    priorotyqueue implementation using Binary Heaps

---
+++ Assets/DataSctructures/PriorityQueue/MaxPriorityQueue.cs
@@ -0,0 +1,41 @@
+﻿namespace UnityAlgorithms
+{
+    public class MaxPriorityQueue <T>
+    {
+        private T[] array = null;
+        private int n;
+
+        public MaxPriorityQueue(int capacity)
+        {
+            array = new T[capacity];
+        }
+
+        public bool IsEmpty()
+        {
+            return n == 0;
+        }
+
+        public T DeleteMax()
+        {
+
+        }
+
+        private void Swin(int k)
+        {
+            while (k > 1 && k < k / 2)
+            {
+
+            }
+
+        }
+
+        private static void Swap<T>(T[] array, int indexA, int indexB)
+        {
+            T valueA = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = valueA;
+        }
+
+    }
+}
+
